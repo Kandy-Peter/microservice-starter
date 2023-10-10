4a11704389fe4595d2b765aa4a2a15681b93dac7
kandy <kandypeter03@gmail.com>
2023-10-10 11:27:20 +0200

    Create microsrv apis for blogs post apis

---
+++ comments/index.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {
+  randomBytes
+} from 'crypto';
+import axios from 'axios'
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+})
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const {
+    content
+  } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({
+    id: commentId,
+    content,
+    status: 'pending'
+  });
+
+  commentsByPostId[req.params.id] = comments;
+  try {
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: commentId,
+        content,
+        status: 'pending',
+        postId: req.params.id
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  res.status(201).send(comments);
+});
+ 
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    try {
+      axios.post('http://event-bus-srv:4005/events', {
+        type: 'CommentUpdated',
+        data: {
+          id,
+          status,
+          postId,
+          content
+        }
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on port 4001   ');
+});
